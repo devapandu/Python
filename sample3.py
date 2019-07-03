0c170aef724a5043dc4f05e3b8e51875bd1ac6e2
Devanand <37849340+devapandu@users.noreply.github.com>
2019-07-03 19:19:14 +0530

    Create sample3.py

---
+++ sample3.py
@@ -0,0 +1,8 @@
+letter = input()
+if(letter>="a" and letter<="z"):
+    if letter in("a", "e", "i", "o", "u"):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("Invalid")
